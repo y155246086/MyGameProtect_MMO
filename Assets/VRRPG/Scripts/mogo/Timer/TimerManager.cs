1338efbfebac66494682dcd70f989b32dfa54e0c
yangqingyan <155246086@qq.com>
2017-04-01 16:28:45 +0800

    SpawnPointGearAgent

---
+++ Assets/VRRPG/Scripts/mogo/Timer/TimerManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace mogo
+{
+    public class TimerManager
+    {
+        public static TimerBehaviour GetTimer(GameObject target)
+        {
+            TimerBehaviour obj = target.GetComponent<TimerBehaviour>();
+            if (obj == null)
+            {
+                obj = target.AddComponent<TimerBehaviour>();
+            }
+            return obj;
+        }
+    }
+}
+
